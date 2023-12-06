23bfb2ddc1a7e6413d17720762ad02c5698c35a2
Rabin Dhamala <rabinkmc@gmail.com>
2023-12-06 22:22:28 +0545

    optimized by solving quadratic equation

--- day6_p2.py
+++ day6_p2.py
@@ -1,20 +1,13 @@
 # Time:        54   70     82     75
 # Distance:   239   1142   1295   1253
-
 times, distances = open("day6.txt").read().strip("\n").split("\n")
-times = int("".join(times.split()[1:]))
-distances = int("".join(distances.split()[1:]))
-
-print(times, distances)
+t = int("".join(times.split()[1:]))
+d = int("".join(distances.split()[1:]))
 
 
-def f(time, hold_time):
-    return (time - hold_time) * hold_time
+det = (t**2 - 4 * d) ** 0.5
+x1 = (t + det) / 2
+x2 = (t - det) / 2
 
-
-count = 0
-
-for time in range(times):
-    if f(times, time) > distances:
-        count += 1
-print(count)
+answer = x1.__floor__() - x2.__ceil__() + 1
+print(answer)
