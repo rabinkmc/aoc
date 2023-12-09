a48c64a4ce0a4b5aa003a35a18f8657e02a9aa12
Rabin Dhamala <rabinkmc@gmail.com>
2023-12-09 13:18:50 +0545

    simpler recursive solution

--- day9.py
+++ day9.py
@@ -4,22 +4,12 @@ inputs = [[int(n) for n in line.split()] for line in data]
 
 
 def next_number(sequence):
-    sequences = [sequence]
-    numbers = sequence
-    while not all(num == 0 for num in numbers):
-        temp = []
-        for i, num in enumerate(numbers[1:]):
-            temp.append(num - numbers[i])
-        numbers = temp
-        sequences.append(numbers)
-
-    sequences[-1].insert(0, 0)
-    for index in range(len(sequences) - 1 - 1, -1, -1):
-        sequence = sequences[index]
-        sequence.insert(0, sequence[0] - sequences[index + 1][0])
-
-    return sequences[0][0]
+    if all(num == 0 for num in sequence):
+        return 0
+    temp = []
+    for i, num in enumerate(sequence[1:]):
+        temp.append(num - sequence[i])
+    return sequence[-1] + next_number(temp)
 
 
-numbers = [next_number(sequence) for sequence in inputs]
-print(sum(numbers))
+print(sum(next_number(sequence) for sequence in inputs))
