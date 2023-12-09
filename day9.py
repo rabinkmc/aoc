beeea82f922d5d13547b44508a52b9bba39af149
Rabin Dhamala <rabinkmc@gmail.com>
2023-12-09 15:37:46 +0545

    refactor

--- day9.py
+++ day9.py
@@ -3,13 +3,11 @@ data = open("day9.txt").read().strip("\n").split("\n")
 inputs = [[int(n) for n in line.split()] for line in data]
 
 
-def next_number(sequence):
-    if all(num == 0 for num in sequence):
+def next_num(seq):
+    if all(num == 0 for num in seq):
         return 0
-    temp = []
-    for i, num in enumerate(sequence[1:]):
-        temp.append(num - sequence[i])
-    return sequence[-1] + next_number(temp)
+    diff = [n - seq[i] for i, n in enumerate(seq[1:])]
+    return seq[-1] + next_num(diff)
 
 
-print(sum(next_number(sequence) for sequence in inputs))
+print(sum(next_num(seq) for seq in inputs))
