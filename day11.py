83f0be3522e00d63a2d98aca84b3a7aacb2d50a1
Rabin Dhamala <rabinkmc@gmail.com>
2023-12-11 15:59:04 +0545

    refactor to Graph class

--- day11.py
+++ day11.py
@@ -1,46 +1,50 @@
-graph = open("day11.txt").read().strip("\n").split("\n")
+from functools import cached_property
 
 
-def empty_space(graph):
-    empty_rows = [rr for rr, row in enumerate(graph) if all(ch == "." for ch in row)]
-    empty_cols = [
-        rr for rr, row in enumerate(zip(*graph)) if all(ch == "." for ch in row)
-    ]
-    return empty_rows, empty_cols
+class Graph:
+    def __init__(self):
+        self.graph = open("day11.txt").read().strip("\n").split("\n")
+
+    @cached_property
+    def empty_rows(self):
+        return [rr for rr, row in enumerate(self.graph) if all(ch == "." for ch in row)]
+
+    @cached_property
+    def empty_cols(self):
+        return [
+            rr
+            for rr, row in enumerate(zip(*self.graph))
+            if all(ch == "." for ch in row)
+        ]
+
+    @cached_property
+    def locations(self):
+        return [
+            (rr, cc)
+            for rr, rows in enumerate(self.graph)
+            for cc, ch in enumerate(rows)
+            if ch == "#"
+        ]
+
+    def dist(self, start, end):
+        r1, c1 = start
+        r2, c2 = end
+        c1, c2 = min(c1, c2), max(c1, c2)
+        total = 0
+
+        for row in range(r1, r2):
+            total += 1000_000 if row in self.empty_rows else 1
+        for col in range(c1, c2):
+            total += 1000_000 if col in self.empty_cols else 1
+
+        return total
+
+    def solution(self):
+        total = 0
+        for i, end in enumerate(self.locations):
+            for start in self.locations[:i]:
+                total += self.dist(start, end)
+        return total
 
 
-def find_galaxies(graph):
-    return [
-        (rr, cc)
-        for rr, rows in enumerate(graph)
-        for cc, ch in enumerate(rows)
-        if ch == "#"
-    ]
-
-
-locations = find_galaxies(graph)
-empty_rows, empty_cols = empty_space(graph)
-
-
-def dist(start, end):
-    r1, c1 = start
-    r2, c2 = end
-    c1, c2 = min(c1, c2), max(c1, c2)
-    total = 0
-    for row in range(r1, r2):
-        total += 1000_000 if row in empty_rows else 1
-    for col in range(c1, c2):
-        total += 1000_000 if col in empty_cols else 1
-
-    return total
-
-
-def sum_of_locations(locations=locations):
-    total = 0
-    for i, end in enumerate(locations):
-        for start in locations[:i]:
-            total += dist(start, end)
-    return total
-
-
-print(sum_of_locations())
+print(Graph().solution())
